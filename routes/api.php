6851b5dc32c3816d5d98dda0ffbc7323bb1ca7f8
Priscila Power <priscilapower92@gmail.com>
2019-03-24 16:47:44 -0300

    crud pools

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('\pool', 'PoolController');
+//Route::middleware('auth')->resource('pools', 'PoolController');
+
+Route::group(['role:admin|manager', 'auth'], function(){
+    Route::resource('pools', 'PoolController');
+});
