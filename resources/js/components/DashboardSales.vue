e8b1d6d90dc2897f54a9c9df45eb992db4827b45
Samuel Rodrigues <me@samoliver.co>
2019-03-24 12:41:34 -0300

    Adds links from dashboard

--- resources/js/components/DashboardSales.vue
+++ resources/js/components/DashboardSales.vue
@@ -1,117 +1,129 @@
 <template>
     <v-container>
-        <v-layout>
-            <v-flex xs12>
-                <div>
-                    <v-card class="mx-3 my-3 cursor-pointer">
-                        <v-card-title
+        <template v-for="pool in poolStats">
+            <v-layout>
+                <v-flex xs12>
+                    <div>
+                        <v-card class="mx-3 my-3 cursor-pointer" @click="helpers.goLink(`/pool/${pool.id}`)">
+                            <v-card-title
                             ><span class="title">Pool #1</span></v-card-title
+                            >
+                            <v-card-text
+                                    class="d-flex justify-content-center align-items-center"
+                            >
+                                <v-flex class="text-xs-center">
+                                    <v-progress-circular
+                                            :width="10"
+                                            :size="200"
+                                            :value="100"
+                                            :color="'yellow'"
+                                    >
+                                        <div class="display-4"
+                                        >12</div
+                                        >
+                                    </v-progress-circular
+                                    >
+                                    <div class="subheading">Potential proposals</div>
+
+                                </v-flex>
+
+                                <v-flex class="text-xs-center">
+                                    <v-progress-circular
+                                            :width="10"
+                                            :size="200"
+                                            :value="75"
+                                            :color="'orange'"
+
+                                    ><span class="display-4"
+                                    >9</span
+                                    ></v-progress-circular
+                                    >
+                                    <div class="subheading">Clients</div>
+                                </v-flex>
+
+                                <v-flex class="text-xs-center">
+                                    <v-progress-circular
+                                            :width="10"
+                                            :size="200"
+                                            :value="75"
+                                            :color="'red'"
+
+                                    ><span class="display-4"
+                                    >9</span
+                                    ></v-progress-circular
+                                    >
+                                    <div class="subheading">Contacts (past 15 days)</div>
+                                </v-flex>
+
+                                <v-flex class="text-xs-center">
+                                    <v-progress-circular
+                                            :width="10"
+                                            :size="200"
+                                            :value="50"
+                                            :color="'purple'"
+
+                                    ><span class="display-4"
+                                    >6</span
+                                    ></v-progress-circular
+                                    >
+                                    <div class="subheading">Proposals</div>
+                                </v-flex>
+                            </v-card-text>
+                        </v-card>
+                    </div>
+                </v-flex>
+            </v-layout>
+            <v-layout>
+                <v-flex xs12>
+                    <v-card class="mx-3 my-3">
+                        <v-card-title
+                        ><span class="title"
+                        >Pool #1 mapped %</span
+                        ></v-card-title
                         >
                         <v-card-text
-                            class="d-flex justify-content-center align-items-center"
+                                class="d-flex justify-content-center align-items-center"
                         >
-                            <v-flex class="text-xs-center">
-                                <v-progress-circular
-                                    :width="10"
-                                    :size="200"
-                                    :value="100"
-                                    :color="'yellow'"
-                                    >
-                                    <div class="display-4"
-                                        >12</div
-                                    >
-                                </v-progress-circular
-                                >
-                                <div class="subheading">Potential proposals</div>
-
-                            </v-flex>
-
-                            <v-flex class="text-xs-center">
-                                <v-progress-circular
-                                    :width="10"
-                                    :size="200"
-                                    :value="75"
-                                    :color="'orange'"
-
-                                ><span class="display-4"
-                                        >9</span
-                                    ></v-progress-circular
-                                >
-                                <div class="subheading">Clients</div>
-                            </v-flex>
-
-                            <v-flex class="text-xs-center">
-                                <v-progress-circular
-                                        :width="10"
-                                        :size="200"
-                                        :value="75"
-                                        :color="'red'"
-
-                                ><span class="display-4"
-                                >9</span
-                                ></v-progress-circular
-                                >
-                                <div class="subheading">Contacts (past 15 days)</div>
-                            </v-flex>
-
-                            <v-flex class="text-xs-center">
-                                <v-progress-circular
-                                        :width="10"
-                                        :size="200"
-                                        :value="50"
-                                        :color="'purple'"
-
-                                ><span class="display-4"
-                                >6</span
-                                ></v-progress-circular
-                                >
-                                <div class="subheading">Proposals</div>
-                            </v-flex>
+                            <v-sparkline
+                                    :value="trend"
+                                    :labels="trend"
+                                    show-labels
+                                    :gradient="['#F44','#000']"
+                                    :smooth="3"
+                                    :padding="10"
+                                    :line-width="2"
+                                    :stroke-linecap="'round'"
+                                    :gradient-direction="'right'"
+                                    auto-draw
+                            ></v-sparkline>
                         </v-card-text>
                     </v-card>
-                </div>
-            </v-flex>
-        </v-layout>
+                </v-flex>
+            </v-layout>
 
-        <v-layout>
-            <v-flex xs12>
-                <v-card class="mx-3 my-3">
-                    <v-card-title
-                        ><span class="title"
-                            >Pool #1 mapped %</span
-                        ></v-card-title
-                    >
-                    <v-card-text
-                        class="d-flex justify-content-center align-items-center"
-                    >
-                        <v-sparkline
-                            :value="trend"
-                            :labels="trend"
-                            show-labels
-                            :gradient="['#F44','#000']"
-                            :smooth="3"
-                            :padding="10"
-                            :line-width="2"
-                            :stroke-linecap="'round'"
-                            :gradient-direction="'right'"
-                            auto-draw
-                        ></v-sparkline>
-                    </v-card-text>
-                </v-card>
-            </v-flex>
-        </v-layout>
+        </template>
+
+
     </v-container>
 </template>
 
 <script>
-export default {
+    import {helpers} from "../plugins/helpers";
+
+    export default {
     name: "DashboardSales",
     data() {
         return {
-            trend: [0, 5, 8, 18, 21, 23, 31, 34]
+            helpers,
+            trend: [0, 5, 8, 18, 21, 23, 31, 34],
+            poolStats: [{id: 1}],
         };
-    }
+    },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+    .cursor-pointer {
+        cursor: pointer;
+    }
+</style>
