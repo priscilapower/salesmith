a2514f505ef6dedad9c195556b26340f63b8531b
Samuel Rodrigues <me@samoliver.co>
2019-03-24 11:40:16 -0300

    Creates manager dashboard

---
+++ resources/js/components/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Salesmith</span>
+        <span class="font-weight-light">Manager Dashboard</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">A button!</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <dashboard-manager></dashboard-manager>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+  import DashboardManager from "./DashboardManager";
+export default {
+  name: 'App',
+  components: {
+      DashboardManager
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
--- resources/js/components/DashboardManager.vue
+++ resources/js/components/DashboardManager.vue
@@ -3,27 +3,110 @@
         <v-layout>
             <v-flex xs4>
                 <v-card class="mx-3 my-3">
-                    <v-card-title>Pools</v-card-title>
-                    <v-card-text>
-                        <v-progress-circular :size="50"></v-progress-circular>
+                    <v-card-title
+                        ><span class="title">Pools</span></v-card-title
+                    >
+                    <v-card-text
+                        class="d-flex justify-content-center align-items-center"
+                    >
+                        <v-progress-circular
+                            :width="10"
+                            :size="200"
+                            :value="100"
+                            ><span class="display-4"
+                                >12</span
+                            ></v-progress-circular
+                        >
                     </v-card-text>
                 </v-card>
             </v-flex>
             <v-flex xs4>
                 <v-card class="mx-3 my-3">
-                    <v-card-title>Potential leads</v-card-title>
+                    <v-card-title
+                        ><span class="title"
+                            >Potential leads</span
+                        ></v-card-title
+                    >
+                    <v-card-text
+                        class="d-flex justify-content-center align-items-center"
+                    >
+                        <v-progress-circular
+                            :width="10"
+                            :size="200"
+                            :value="100"
+                            ><span class="display-4"
+                                >12</span
+                            ></v-progress-circular
+                        >
+                    </v-card-text>
                 </v-card>
             </v-flex>
             <v-flex xs4>
                 <v-card class="mx-3 my-3">
-                    <v-card-title>Avg. pool mapped</v-card-title>
+                    <v-card-title
+                        ><span class="title"
+                            >Avg% pool mapped</span
+                        ></v-card-title
+                    >
+                    <v-card-text
+                        class="d-flex justify-content-center align-items-center"
+                    >
+                        <v-progress-circular
+                            :width="10"
+                            :size="200"
+                            :value="100"
+                            ><span class="display-4"
+                                >12</span
+                            ></v-progress-circular
+                        >
+                    </v-card-text>
                 </v-card>
             </v-flex>
         </v-layout>
         <v-layout>
             <v-flex xs6>
                 <v-card class="mx-3 my-3">
-                    <v-card-title>Avg. mapped to proposal</v-card-title>
+                    <v-card-title
+                        ><span class="title"
+                            >Avg% mapped to proposal</span
+                        ></v-card-title
+                    >
+                    <v-card-text
+                        class="d-flex justify-content-center align-items-center"
+                    >
+                        <v-progress-circular
+                            :width="10"
+                            :size="200"
+                            :value="100"
+                            ><span class="display-4"
+                                >12</span
+                            ></v-progress-circular
+                        >
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+            <v-flex xs6>
+                <v-card class="mx-3 my-3">
+                    <v-card-title
+                        ><span class="title"
+                            >Contacts/day (last 15 days)</span
+                        ></v-card-title
+                    >
+                    <v-card-text
+                        class="d-flex justify-content-center align-items-center"
+                    >
+                        <v-sparkline
+                            :value="trend"
+                            :height="118"
+                            :gradient="['#000', '#F44']"
+                            :smooth="3"
+                            :padding="3"
+                            :line-width="3"
+                            :stroke-linecap="'round'"
+                            :gradient-direction="'right'"
+                            auto-draw
+                        ></v-sparkline>
+                    </v-card-text>
                 </v-card>
             </v-flex>
         </v-layout>
@@ -31,11 +114,14 @@
 </template>
 
 <script>
-    export default {
-        name: "DashboardManager"
+export default {
+    name: "DashboardManager",
+    data() {
+        return {
+            trend: [6, 7, 8, 3, 5, 9, 3, 1]
+        };
     }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
